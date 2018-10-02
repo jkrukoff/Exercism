eb24bb90b63837487174f047ded60e6c06b46444
John Krukoff <john@krukoff.us>
2018-10-02 10:10:05 -0600

    Roman numerals.

--- erlang/roman-numerals/src/roman_numerals.erl
+++ erlang/roman-numerals/src/roman_numerals.erl
@@ -6,7 +6,7 @@
 
 -spec numerals(integer()) -> string().
 numerals(Number) when Number =< 4999 ->
-    to_roman(digits(Number)).
+    roman_digits(Number, roman_values(), []).
 
 -spec test_version() -> integer().
 test_version() ->
@@ -14,47 +14,28 @@ test_version() ->
 
 %% Internal
 
-digits(Number) ->
-    digits(Number, []).
+roman_values() ->
+    roman_values(1, "IVXLCDM", []).
 
-% Number as list of digits, from most significant to least.
-digits(0, Acc) ->
-    Acc;
-digits(Number, Acc) ->
-    digits(Number div 10, [Number rem 10 | Acc]).
+% Not general purpose, sensitive to the length of the string of
+% available digits.
+roman_values(Value, [One, Five, Ten | Next], Acc) ->
+    NextAcc = [{Value * 9, [One, Ten]},
+               {Value * 5, [Five]},
+               {Value * 4, [One, Five]},
+               {Value, [One]} | Acc],
+    roman_values(Value * 10, [Ten | Next], NextAcc);
+roman_values(Value, [One | []], Acc) ->
+    [{Value, [One]} | Acc].
 
-roman_digit(0, {_Ones, _Fives, _Tens}) ->
-    [];
-roman_digit(1, {Ones, _Fives, _Tens}) ->
-    [Ones];
-roman_digit(2, {Ones, _Fives, _Tens}) ->
-    [Ones, Ones];
-roman_digit(3, {Ones, _Fives, _Tens}) ->
-    [Ones, Ones, Ones];
-roman_digit(4, {Ones, Fives, _Tens}) ->
-    [Ones, Fives];
-roman_digit(5, {_Ones, Fives, _Tens}) ->
-    [Fives];
-roman_digit(6, {Ones, Fives, _Tens}) ->
-    [Fives, Ones];
-roman_digit(7, {Ones, Fives, _Tens}) ->
-    [Fives, Ones, Ones];
-roman_digit(8, {Ones, Fives, _Tens}) ->
-    [Fives, Ones, Ones, Ones];
-roman_digit(9, {Ones, _Fives, Tens}) ->
-    [Ones, Tens].
-
-% Number as iolist of roman digits, from most significant to least.
-to_roman(Digits) ->
-    RomanNumerals = [{"I", "V", "X"},
-                     {"X", "L", "C"},
-                     {"C", "D", "M"},
-                     {"M", undefined, undefined}],
-    lists:flatten(to_roman(lists:reverse(Digits), RomanNumerals, [])).
-
-to_roman([], _Roman, Acc) ->
-    Acc;
-to_roman(_Digits, [], _Acc) ->
-    throw({toolarge, "Number too large to convert."});
-to_roman([Digit | NextDigit], [RomanDigits | NextRomanDigits], Acc) ->
-    to_roman(NextDigit, NextRomanDigits, [roman_digit(Digit, RomanDigits) | Acc]).
+% Given a list of digit values from largest to smallest, we construct
+% an iolist of roman digits for each value smaller than the current
+% number, then subtract the value of the digit added and continue.
+roman_digits(0, _Values, Acc) ->
+    lists:flatten(lists:reverse(Acc));
+roman_digits(_Number, [], _Acc) ->
+    throw({invalid_values, "Invalid roman values given, is there no 1 value?"});
+roman_digits(Number, [{Value, _Digit} | NextValue], Acc) when Number - Value < 0 ->
+    roman_digits(Number, NextValue, Acc);
+roman_digits(Number, [{Value, Digit} | _] = Values, Acc) ->
+    roman_digits(Number - Value, Values, [Digit | Acc]).
