40642b1ce086f349a60e01e1f14449d694a6df43
John Krukoff <john@krukoff.us>
2018-10-20 23:15:42 -0600

    protein_translation

--- elixir/protein-translation/protein_translation.exs
+++ elixir/protein-translation/protein_translation.exs
@@ -1,9 +1,33 @@
 defmodule ProteinTranslation do
+  @rna_error "invalid RNA"
+  @codon_error "invalid codon"
+
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
   @spec of_rna(String.t()) :: {atom, list(String.t())}
   def of_rna(rna) do
+    of_rna(rna, [])
+  end
+
+  defp of_rna("", acc) do
+    {:ok, Enum.reverse(acc)}
+  end
+
+  defp of_rna(<<codon1 :: utf8, codon2 :: utf8, codon3 :: utf8, rna :: binary>>, acc) do
+    codon = <<codon1, codon2, codon3>>
+    case of_codon(codon) do
+      {:error, _message} ->
+        {:error, @rna_error}
+      {:ok, "STOP"} ->
+        of_rna("", acc)
+      {:ok, protein} ->
+        of_rna(rna, [protein | acc])
+    end
+  end
+
+  defp of_rna(_rna, _acc) do
+    {:error, @rna_error}
   end
 
   @doc """
@@ -28,6 +52,22 @@ defmodule ProteinTranslation do
   UGA -> STOP
   """
   @spec of_codon(String.t()) :: {atom, String.t()}
-  def of_codon(codon) do
-  end
+  def of_codon("UGU"), do: {:ok, "Cysteine"}
+  def of_codon("UGC"), do: {:ok, "Cysteine"}
+  def of_codon("UUA"), do: {:ok, "Leucine"}
+  def of_codon("UUG"), do: {:ok, "Leucine"}
+  def of_codon("AUG"), do: {:ok, "Methionine"}
+  def of_codon("UUU"), do: {:ok, "Phenylalanine"}
+  def of_codon("UUC"), do: {:ok, "Phenylalanine"}
+  def of_codon("UCU"), do: {:ok, "Serine"}
+  def of_codon("UCC"), do: {:ok, "Serine"}
+  def of_codon("UCA"), do: {:ok, "Serine"}
+  def of_codon("UCG"), do: {:ok, "Serine"}
+  def of_codon("UGG"), do: {:ok, "Tryptophan"}
+  def of_codon("UAU"), do: {:ok, "Tyrosine"}
+  def of_codon("UAC"), do: {:ok, "Tyrosine"}
+  def of_codon("UAA"), do: {:ok, "STOP"}
+  def of_codon("UAG"), do: {:ok, "STOP"}
+  def of_codon("UGA"), do: {:ok, "STOP"}
+  def of_codon(_), do: {:error, @codon_error}
 end
