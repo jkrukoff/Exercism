c888950ff9f0842bef81281499984f26dc1ee2d0
John Krukoff <john@krukoff.us>
2018-11-07 15:17:57 -0700

    binary-search

--- elixir/binary-search/binary_search.exs
+++ elixir/binary-search/binary_search.exs
@@ -18,6 +18,29 @@ defmodule BinarySearch do
   """
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
+  def search({}, _key) do
+    :not_found
+  end
+
   def search(numbers, key) do
+    partition(numbers, key, 0, tuple_size(numbers) - 1)
+  end
+
+  defguardp midpoint(first, last) when div(last - first, 2) + first
+
+  defp partition(numbers, key, first, last) when elem(numbers, midpoint(first, last)) == key do
+    {:ok, midpoint(first, last)}
+  end
+
+  defp partition(_numbers, _key, first, first) do
+    :not_found
+  end
+
+  defp partition(numbers, key, first, last) when elem(numbers, midpoint(first, last)) < key do
+    partition(numbers, key, midpoint(first, last) + 1, last)
+  end
+
+  defp partition(numbers, key, first, last) when elem(numbers, midpoint(first, last)) > key do
+    partition(numbers, key, first, midpoint(first, last) - 1)
   end
 end
